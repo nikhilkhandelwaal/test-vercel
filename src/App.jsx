46b7a54ed93ae74b016c33c705f0de4f72f3d598
nikhilkhandelwaal <nikhilcoder061@gmail.com>
2026-04-04 00:19:09 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ function App() {
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
+          <h1>Hello Everyone</h1>
         </div>
         <button
           className="counter"
